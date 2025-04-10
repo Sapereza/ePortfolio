596586105608a01681e72103d3eace5ec361ce14
sapereza <s.perezanderson@outlook.com>
2025-04-10 09:56:19 -0400

    I added badge images to the certification secton. However, there is much editing to do so that it animates how I want it to. For now the main information is up.

--- src/components/AboutDetails/AboutSectionOne.jsx
+++ src/components/AboutDetails/AboutSectionOne.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from 'react';
+import AWSCert from '/src/assets/AWSCert.png';
+import GoogleITCert from '/src/assets/GoogleITCert.png';
 
 const AboutSectionOne = () => {
   return (
+    //Adjust this div section by have a crousel & add hover over badge - Possibly add link to Credly for each badge
     <div>
-        <p>
-           My certification badges will be displayed here soon. For now click my LinkedIn link on the left, for mobile the link is on the bottom, to view the badges on my profile. 
-        </p>
+        <div className='flex items-center'>
+          <img src={AWSCert} className='w-32 h-32 hover:scale-110 duration-500'>
+          </img>
+
+          <img src={GoogleITCert} className='w-32 h-32 hover:scale-110 duration-500'> 
+          </img>
+          
+        </div>
     </div>
   )
 }
