67191da898f7217c1ee74644ea1e36770d1f4d5f
sapereza <s.perezanderson@outlook.com>
2025-04-09 20:20:39 -0400

    updated text on about section one

--- src/components/AboutDetails/AboutSectionOne.jsx
+++ src/components/AboutDetails/AboutSectionOne.jsx
@@ -4,7 +4,7 @@ const AboutSectionOne = () => {
   return (
     <div>
         <p>
-           My certification badges will be displayed here soon. For now click my LinkedIn link on the left to view the badges on my profile.
+           My certification badges will be displayed here soon. For now click my LinkedIn link on the left to view the badges on my profile(Links viewable on large screens and desktop at the moment). 
         </p>
     </div>
   )
